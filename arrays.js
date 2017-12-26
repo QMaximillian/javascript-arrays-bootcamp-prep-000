da1c1ce7c2ed22b7a25dce99edd5e94c6e69b7e6
Quinn Lashinsky <quinnlashinsky@gmail.com>
2017-12-26 16:29:32 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -35,7 +35,7 @@ function destructivelyRemoveElementFromBeginningOfArray(array) {
 }
 
 function removeElementFromBeginningOfArray(array){
-  var newArray = array.slice(1)
+  var newArray = array.slice(0)
   return newArray
 
 
